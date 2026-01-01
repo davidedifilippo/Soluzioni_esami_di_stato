7257706d77eb8e52d2bee3b03107939fbb10eaa3
Ing. Di Filippo <davidedifilippo@gmail.com>
2026-01-01 16:30:09 +0100

    Refactor biscuit automation code
    Aggiunta la sezione di immissione di acqua

--- Simulazioni/sim_15_marzo_2023_automazione_biscotti.ino
+++ Simulazioni/sim_15_marzo_2023_automazione_biscotti.ino
@@ -1,29 +1,30 @@
 #include <Arduino.h>
 
+/*Definizione dei pin digitali */
 const int startButtonPin = 4; //Pulsante di Start
-
-const int pinSensoreBiscotti = 2; //Pulsante di Start
-const int pinSensoreFlussoAcqua = 3; //Pulsante di Start
+const int pinSensoreBiscotti = 2; //il cambio di stato innesca un interrupt del micro
+const int pinSensoreFlussoAcqua = 3; //il cambio di stato innesca un interrupt del micro
 const int pincontrolloImmissionmeFarina = 5; 
+const int scivoloFarina = 6;
 
+/*Definizione dei pin Analogici */
+const int pinAnalogicoBilancia = A0;
 
-
-
-int contaB = 0;
+/*Definizione delle variabili*/
+int contatoreBiscotti = 0;
 int contaImpulsiAcqua = 0;
 bool impastoFinito = true;
 byte pesoFarina =0;  
 
-
-
-
+//Ogni volta che cade un biscotto arriva un impulso di conteggio
 void contaBiscotti()
 {
-     contaB++;
+     contatoreBiscotti++;
     }
 
 
-void contaImpulsi()
+//ogni 100 impulsi abbiamo un litro di acqua immesso nell'impasto
+void contaImpulsiFlussimetro()
 {
      contaImpulsiAcqua++;
     }
@@ -32,49 +33,39 @@ void contaImpulsi()
 void setup() {
 
 pinMode(startButtonPin, INPUT);
-
-
-
- 
-
 pinMode(pinSensoreBiscotti, INPUT);
+pinMode(pinSensoreFlussoAcqua, INPUT);
+pinMode(scivoloFarina, OUTPUT);
 
-//ad ogni rilevazione di passaggio di un biscotto incrementa il contatore
-
-attachInterrupt(digitalPinToInterrupt(pinSensoreBiscotti), contaBiscotti, RISING); 
-
-pinMode(pinSensoreBiscotti, INPUT);
-
-//ad ogni rilevazione di passaggio di un biscotto incrementa il contatore
-
-attachInterrupt(digitalPinToInterrupt(pinSensoreFlussoAcqua), contaImpulsi, RISING); 
+//associamo le routine di interrupt ai piedini sensibili ai fronti di discesa/salita
+attachInterrupt(digitalPinToInterrupt(pinSensoreBiscotti), contaBiscotti, FALLING); 
+attachInterrupt(digitalPinToInterrupt(pinSensoreFlussoAcqua), contaImpulsiFlussimetro, FALLING); 
 
 }
 
 void loop() {
   
-while(!digitalRead(startButtonPin)); //pull-up resistor -> se non premuto HIGH   
+while(digitalRead(startButtonPin)); //pull-up resistor -> se non premuto HIGH   
 
-
-
+//Controllo se devo preparare l'impasto 
 if(impastoFinito){
+digitalWrite(pincontrolloImmissionmeFarina, HIGH); //Scarico la farina sulla bilancia
 
-digitalWrite(pincontrolloImmissionmeFarina, HIGH);
-
-do{
-
-int x = analogRead(0);
-
-pesoFarina = (x/1023)*100;
-
-
-} while(pesoFarina<80);
+while(pesoFarina<80);
+{
+  int x = analogRead(A0);
+  pesoFarina = (x/1023)*100; //Peso massimo 100 Newton
+} 
 
 digitalWrite(pincontrolloImmissionmeFarina, LOW);
-digitalWrite(scivoloFarina, HIGH);
-digitalWrite(pinSensoreFlussoAcqua, HIGH);
-while(contaImpulsiAcqua<=500);
- digitalWrite(pinSensoreFlussoAcqua, LOW);    
+
+digitalWrite(scivoloFarina, HIGH); //Inserisco la farina nel miscelatore
+digitalWrite(pinSensoreFlussoAcqua, HIGH); // Inserisco l'acqua nel miscelatore
+
+while(contaImpulsiAcqua<=500); //Continuo a immettere acqua fino a raggiungere 5 Litri
+ 
+digitalWrite(pinSensoreFlussoAcqua, LOW); //Basta acqua, tocca al lievito
+
      
      
 }
